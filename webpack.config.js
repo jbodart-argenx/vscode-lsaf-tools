e434719f8b84bd86db1064a4d28c7e0a40c56a44
Jean-Michel Bodart <jbodart@argenx.com>
2025-02-10 15:23:35 +0100

    Update webpack.config.js
    include optimizations like code splitting and minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-   mode: 'production', // Set the mode to 'production' or 'development'
-   target: 'webworker', // Use 'webworker' for web extensions
+   mode: 'production', // 'production' or 'development'
+   target: 'webworker', // 'webworker' for web extensions
    entry: {
       extension: './src/extension.js',
-      'web/extension': './src/web/extension.js' // Add a new entry point for the web extension
+      'web/extension': './src/web/extension.js'
    },
    output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',  // Output filename pattern - replaced with the name of each entry point defined in the entry section. In this case, there are two entry points: 'extension' and 'web/extension'.
+      filename: '[name].js',  // Replaced pattern with the name of each entry point: 'extension' and 'web/extension'
       libraryTarget: 'commonjs2',
    },
    resolve: {
@@ -21,4 +23,18 @@ module.exports = {
    externals: {
       vscode: 'commonjs vscode',
    },
+   optimization: {
+      splitChunks: {
+         chunks: 'all',
+      },
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+   },
+   plugins: [
+      new BundleAnalyzerPlugin({
+         analyzerMode: 'static',
+         openAnalyzer: false,
+         reportFilename: 'bundle-report.html',
+      }),
+   ],
 };
