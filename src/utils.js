d52e631516c44fdbde761d8531663f5602c10713
Jean-Michel Bodart <jbodart@argenx.com>
2025-02-11 12:33:42 +0100

    Update utils.js
    Add async function getOppositeEndpointUri()

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 const vscode = require('vscode');
+let defaultEndpoints = require('./endpoints').defaultEndpoints;
+console.log('Default Endpoints:', defaultEndpoints);
 
 const { uriFromString } = require('./uri');
 
@@ -30,4 +32,38 @@ async function copyFileOrFolderUri(fileOrFolder) {
    }
 }
 
-module.exports = { copyFileOrFolderUri };
+async function getOppositeEndpointUri(fileOrFolder) {
+   // Get the opposite endpoint from the defaultEndpoints
+   const config = vscode.workspace.getConfiguration("vscode-lsaf-tools");
+   const endpoints = config.get('defaultEndpoints') || [];
+   if (endpoints) {
+      const fileOrFolderUri = uriFromString(fileOrFolder);
+      // Find the endpoints that match and don't match the fileOrFolderUri
+      const endpointIndex = defaultEndpoints.findIndex(ep => fileOrFolderUri.toString().startsWith(ep.uri.toString()));
+      if (endpointIndex >= 0) {
+         const endpoint1 = defaultEndpoints[endpointIndex];
+         const endpoint1RelPath = fileOrFolderUri.toString().replace(endpoint1.uri.toString(), '').replace(/^\//, '');
+         const otherEndpoints = defaultEndpoints.filter((ep, idx) => idx !== endpointIndex);
+         const selectedOtherEndpointLabel = await vscode.window.showQuickPick(otherEndpoints.map(ep => ep.label), {
+            placeHolder: "Choose an endpoint",
+            canPickMany: false,
+         });
+         if (selectedOtherEndpointLabel) {
+            const otherEndpoint = otherEndpoints.find(ep => ep.label === selectedOtherEndpointLabel);
+            const otherEndpointUri = vscode.Uri.joinPath(otherEndpoint.uri, endpoint1RelPath);
+            console.log(`(getOppositeEndpoint) Opposite endpoint for ${fileOrFolder} is ${otherEndpointUri}`);
+            vscode.window.showInformationMessage(`Opposite endpoint for ${fileOrFolder} is ${otherEndpointUri}`);
+            return otherEndpointUri;
+         }
+      } else {
+         vscode.window.showWarningMessage(`Failed to get opposite endpoint for ${fileOrFolder}: no matching endpoint found.`);
+         console.error(`(getOppositeEndpoint) Failed to get opposite endpoint for ${fileOrFolder}: no matching endpoint found.`);
+      }
+   } else {
+      vscode.window.showWarningMessage(`Failed to get opposite endpoint for ${fileOrFolder}: no endpoints defined.`);
+      console.error(`(getOppositeEndpoint) Failed to get opposite endpoint for ${fileOrFolder}: no endpoints defined.`);
+   }
+   return null;
+}
+
+module.exports = { copyFileOrFolderUri, getOppositeEndpointUri };
