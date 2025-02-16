2080e4188dd6047f25d9fd7041ad31bf34c89612
Jean-Michel Bodart <jbodart@argenx.com>
2025-02-16 19:04:30 +0100

    Update utils.test.js
    Enhance tests for getFileOrFolderUri to cover additional & more appropriate scenarios

--- test/utils.test.js
+++ test/utils.test.js
@@ -21,11 +21,31 @@ suite('getFileOrFolderUri', () => {
       sandbox.restore();
    });
 
-   test('should return an array of URIs when fileOrFolder is an array', () => {
+   test('should return an array of URIs when fileOrFolder is an array of strings', () => {
       const uris = ['file:///path/to/file1', 'file:///path/to/file2'];
       const result = getFileOrFolderUri(uris);
       expect(result).to.be.an('array');
       expect(result).to.have.lengthOf(2);
+      result.forEach(uri => {
+         expect(uri).to.be.an.instanceof(vscode.Uri);
+      });
+   });
+
+   test('should return an array of URIs when fileOrFolder is an array of vscode.Uri', () => {
+      const uris = [vscode.Uri.file('/path/to/file1'), vscode.Uri.file('/path/to/file2')];
+      const result = getFileOrFolderUri(uris);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(2);
+      result.forEach(uri => {
+         expect(uri).to.be.an.instanceof(vscode.Uri);
+      });
+   });
+
+   test('should return a single URI when fileOrFolder is a single vscode.Uri', () => {
+      const uri = vscode.Uri.file('/path/to/file');
+      const result = getFileOrFolderUri(uri);
+      expect(result).to.be.an.instanceof(vscode.Uri);
+      expect(result.toString()).to.equal(uri.toString());
    });
 
    test('should return the active text editor URI when fileOrFolder is null and activeTextEditor is defined', () => {
@@ -34,6 +54,7 @@ suite('getFileOrFolderUri', () => {
 
       const result = getFileOrFolderUri(null);
       expect(result).to.equal(mockUri);
+      expect(result).to.be.an.instanceof(vscode.Uri);
    });
 
    test('should return the active editor document URI when fileOrFolder is null and activeEditor.document.uri is defined', function () {
@@ -46,6 +67,7 @@ suite('getFileOrFolderUri', () => {
 
       const result = getFileOrFolderUri(null);
       expect(result).to.equal(mockUri);
+      expect(result).to.be.an.instanceof(vscode.Uri);
    });
 
    test('should return a URI when fileOrFolder is a string', () => {
@@ -53,5 +75,21 @@ suite('getFileOrFolderUri', () => {
       const result = getFileOrFolderUri(uriString);
       expect(result).to.be.an.instanceof(vscode.Uri);
       expect(result.toString()).to.equal(uriString);
+   });
+
+   test('should return null when fileOrFolder is undefined', () => {
+      const result = getFileOrFolderUri(undefined);
+      expect(result).to.be.null;
+   });
+
+   test('should return a file URI when fileOrFolder is an invalid string', () => {
+      const result = getFileOrFolderUri('invalid-uri');
+      expect(result).to.be.an.instanceof(vscode.Uri);
+      expect(result.scheme).to.equal('file');
+   });
+
+   test('should return null when fileOrFolder is an empty string', () => {
+      const result = getFileOrFolderUri('');
+      expect(result).to.be.null;
    });
 });
