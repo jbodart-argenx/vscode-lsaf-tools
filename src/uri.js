3bed14a1677c3daecaa09df7902e0c0912f67f3a
Jean-Michel Bodart <jbodart@argenx.com>
2025-04-18 23:05:17 +0200

    Update uri.js
    fix existsUri() function to use bitwise AND for type checking and return a Boolean value

--- src/uri.js
+++ src/uri.js
@@ -174,7 +174,7 @@ async function existsUri(fileUri, type = null, stat = vscode.workspace.fs.stat) 
       try {
          let fileStat = await stat(fileUri);  // async operation
          if (fileStat) exists = true;
-         if (type != null) exists = (fileStat.type === type);
+            if (type != null) exists = Boolean(fileStat.type & type);  // & : bitwise AND operation
       } catch (error) {
          if (error) console.log(`Uri does not exist: ${fileUri},`, error?.code);
       }
